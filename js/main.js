e7bf7cb9fe5af8ea8a4a0ce8ace375cab2d693aa
eundonghyeok <mephisto7191@naver.com>
2025-02-07 17:17:56 +0900

    visual 순차 애니메이션 구현

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
+// 검색 효과 JS
 const searchEl = document.querySelector(".search");
 const searchInputEl = searchEl.querySelector("input");
 
-searchEl.addEventListener("click", function () {
+searchEl.addEventListener("click", () => {
   searchInputEl.focus();
 });
 
-searchInputEl.addEventListener("focus", function () {
+searchInputEl.addEventListener("focus", () => {
   searchEl.classList.add("focused");
   searchInputEl.setAttribute("placeholder", "통합검색");
 });
 
-searchInputEl.addEventListener("blur", function () {
+searchInputEl.addEventListener("blur", () => {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
 });
 
+// 배지 효과 JS
 const badgeEl = document.querySelector("header .badges");
 
 window.addEventListener(
@@ -36,3 +38,12 @@ window.addEventListener(
     }
   }, 300)
 );
+
+// Fade In 효과 JS
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+fadeEls.forEach((fadeEl, idx) => {
+  gsap.to(fadeEl, 1, {
+    delay: 0.7 * (idx + 1),
+    opacity: 1,
+  });
+});
