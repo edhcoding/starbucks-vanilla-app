2e97adb06f091f52e735e5312be13e143e4cb743
eundonghyeok <mephisto7191@naver.com>
2025-02-09 16:58:08 +0900

    chore: 로그인 페이지 세팅

---
+++ js/common.js
@@ -0,0 +1,21 @@
+// 검색 효과 JS
+const searchEl = document.querySelector(".search");
+const searchInputEl = searchEl.querySelector("input");
+
+searchEl.addEventListener("click", () => {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener("focus", () => {
+  searchEl.classList.add("focused");
+  searchInputEl.setAttribute("placeholder", "통합검색");
+});
+
+searchInputEl.addEventListener("blur", () => {
+  searchEl.classList.remove("focused");
+  searchInputEl.setAttribute("placeholder", "");
+});
+
+// 자동 년도 계산
+const thisYear = document.querySelector(".this-year");
+thisYear.textContent = new Date().getFullYear();
--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-// 검색 효과 JS
-const searchEl = document.querySelector(".search");
-const searchInputEl = searchEl.querySelector("input");
-
-searchEl.addEventListener("click", () => {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener("focus", () => {
-  searchEl.classList.add("focused");
-  searchInputEl.setAttribute("placeholder", "통합검색");
-});
-
-searchInputEl.addEventListener("blur", () => {
-  searchEl.classList.remove("focused");
-  searchInputEl.setAttribute("placeholder", "");
-});
-
 // 배지 효과 JS
 const badgeEl = document.querySelector("header .badges");
 
@@ -154,7 +136,3 @@ new Swiper(".awards .swiper-container", {
     nextEl: ".awards .swiper-next",
   },
 });
-
-// 자동 년도 계산
-const thisYear = document.querySelector(".this-year");
-thisYear.textContent = new Date().getFullYear();
