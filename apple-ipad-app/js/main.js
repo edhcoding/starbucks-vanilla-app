e6545532bdb1e55332c2fe0d20ffc3346753186d
eundonghyeok <mephisto7191@naver.com>
2025-02-15 14:32:32 +0900

    power 섹션 구현 - sprite image 애니메이션 + JS, IntersectionObserver API

--- apple-ipad-app/js/main.js
+++ apple-ipad-app/js/main.js
@@ -60,3 +60,37 @@ function hideSearch() {
   searchDelayEls.reverse();
   searchInputEl.value = "";
 }
+
+// 프레임 위치 및 속성값 JS로 제어하기
+let x = 0;
+let y = 0;
+let frames = "";
+for (let i = 0; i < 60; i += 1) {
+  // frames += "0% { background-position: 0 0; }"
+  // frames += `${(100 / 60 * i).toFixed(2)}% { background-position: ${x}px ${y}px; }<br />` // HTML으로 출력!
+  frames += `${((100 / 60) * i).toFixed(2)}% { background-position: ${x}${
+    x === 0 ? "" : "px"
+  } ${y}${y === 0 ? "" : "px"}; }\n`;
+  if (x <= -500) {
+    x = 0;
+    y -= 100;
+    continue; // 현재 반복을 종료하고 다음 반복으로 넘어가기!
+  }
+  x -= 100;
+}
+// document.body.innerHTML = frames // HTML으로 출력!
+console.log(frames);
+
+// Intersection Observer API - 요소의 가시성 관찰
+const io = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.add("show");
+  });
+});
+
+const infoEls = document.querySelectorAll(".info");
+infoEls.forEach((el) => {
+  io.observe(el);
+});
