6cd9605b1bb112fd747eb8a55cf11335665db787
eundonghyeok <mephisto7191@naver.com>
2025-02-23 14:17:13 +0900

    상태 관리 패턴 적용

--- vanillajs-movie-app/src/core/donghyeok.js
+++ vanillajs-movie-app/src/core/donghyeok.js
@@ -62,3 +62,34 @@ export function createRouter(routes) {
     routeRender(routes); // popstate 이벤트는 처음에는 동작하지 않기 때문에 최초에 한 번 호출 해줘야함
   };
 }
+
+///// Store /////
+export class Store {
+  constructor(state) {
+    this.state = {};
+    this.observers = {};
+    for (const key in state) {
+      // 객체 데이터의 속성을 정의하거나 수정할 때 사용하는 메서드임
+      // Object.defineProperty(객체, 속성이름, 디스크립터 속성(가장 중요))
+      // 디스크립터 속성 종류 - value: 속성의 값, writable: 값 수정 가능 여부, enumerable: 속성의 열거 가능 여부, configurable: 속성 삭제 및 디스크립터 수정 가능 여부, get: getter 함수, set: setter 함수
+      // 결국 여기서 Object.definePropperty를 사용하는 이유는 정의하고자 하는 각각의 데이터들의 어떠한 새로운 값이 할당될 때마다 필요한 함수를 실행 시켜주기 위함 - 데이터를 감시하도록 만들어줄 수 있음
+      Object.defineProperty(this.state, key, {
+        // getter 함수는 값을 읽을 때 호출되는 함수, this.state라는 객체 데이터에 지정하는 키 값을 사용할 때 동작하는 함수
+        get: () => state[key],
+        // setter 함수는 값을 수정할 때 호출되는 함수, this.state라는 객체 데이터에 특정 속성을 할당할 때 즉, 새롭게 지정할 때 동작하는 함수
+        set: (value) => {
+          state[key] = value;
+          this.observers[key].forEach((observer) => observer(value));
+        },
+      });
+    }
+  }
+
+  // 데이터 상태 변경 시 실행되는 함수
+  // 콜백 함수를 배열로 저장해서 여러개의 콜백 함수를 실행 시킬 수 있도록 함 - 하나만 실행시키도록 할 경우 나중에 작성한 컴포넌트가 덮어씌워버리는 현상 생김
+  subscribe(key, cb) {
+    Array.isArray(this.observers[key])
+      ? this.observers[key].push(cb)
+      : (this.observers[key] = [cb]); // 콜백 함수를 하나의 배열 데이터 형식으로 저장
+  }
+}
---
+++ vanillajs-movie-app/src/store/message.js
@@ -0,0 +1,5 @@
+import { Store } from "../core/donghyeok";
+
+export default new Store({
+  message: "Hello World",
+});
