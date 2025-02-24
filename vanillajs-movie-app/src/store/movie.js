49d4534f2c3c5178086f5c4bc0e3189d5db2feba
eundonghyeok <mephisto7191@naver.com>
2025-02-24 16:14:29 +0900

    try catch 에러처리, 데이터에 따른 메시지 처리

--- vanillajs-movie-app/src/store/movie.js
+++ vanillajs-movie-app/src/store/movie.js
@@ -8,6 +8,7 @@ const store = new Store({
   movies: [],
   pageMax: 1,
   loading: false,
+  message: "Search for the movie title!",
 });
 
 export default store;
@@ -17,18 +18,30 @@ export const searchMovies = async (page) => {
   store.state.page = page;
 
   // 검색을 통해 새로운 영화 목록을 불러오면 기존 영화 목록을 초기화
-  if (page === 1) store.state.movies = [];
+  if (page === 1) {
+    store.state.movies = [];
+    store.state.message = "";
+  }
 
-  // https://www.omdbapi.com/
-  // OBDb API는 http로 알려주고 있어서 localhost에서 개발할때는 문제가 없지만, 나중에 실제 vercel을 통해 배포하게되면 https로 시작하게되는데
-  // https => http로 더 보안이 낮은 곳으로 데이터를 요청하는 것은 브라우저마다 허용되지 않을 수 있음
-  const res = await fetch(
-    `https://www.omdbapi.com?apikey=${APIKEY}&s=${store.state.searchText}&page=${page}`
-  );
-  const { Search, totalResults } = await res.json();
+  try {
+    // https://www.omdbapi.com/
+    // OBDb API는 http로 알려주고 있어서 localhost에서 개발할때는 문제가 없지만, 나중에 실제 vercel을 통해 배포하게되면 https로 시작하게되는데
+    // https => http로 더 보안이 낮은 곳으로 데이터를 요청하는 것은 브라우저마다 허용되지 않을 수 있음
+    const res = await fetch(
+      `https://www.omdbapi.com?apikey=${APIKEY}&s=${store.state.searchText}&page=${page}`
+    );
+    const { Search, totalResults, Response, Error } = await res.json();
 
-  store.state.movies = [...store.state.movies, ...Search];
-  // totalResults는 문자 데이터이기 때문에 숫자로 변환
-  store.state.pageMax = Math.ceil(Number(totalResults) / 10);
-  store.state.loading = false;
+    if (Response === "True") {
+      store.state.movies = [...store.state.movies, ...Search];
+      // totalResults는 문자 데이터이기 때문에 숫자로 변환
+      store.state.pageMax = Math.ceil(Number(totalResults) / 10);
+    } else {
+      store.state.message = Error;
+    }
+  } catch (error) {
+    console.log("Data fetching error: ", error);
+  } finally {
+    store.state.loading = false;
+  }
 };
